e173bfd0849b8bc0ee59ab04184076b07eb5a162
WildSteamUK <wildsteamuk@gmail.com>
2025-04-07 21:51:04 +0100

    Update base.njk layout with full homepage design

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
+  // Allow Netlify CMS and uploads to be copied over to the final _site directory
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("uploads");
 
   return {
     dir: {
